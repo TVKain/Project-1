875c8f8b0dd6e51b9c2278d7e518509e2fca1ba9
kain <tvkain.it@gmail.com>
2022-10-29 19:02:56 +0700

    Add Array List Datastructures

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
-#include "test.hpp"
+#include <iostream>
+#include <string>
 
-int main() 
-{
-    test a;
+#include "array_list.hpp"
 
-    a.print();
+void selectionSort(ds::array_list<int> &a) {
+    for (int i = 0; i < a.size() - 1; ++i) {
+        for (int j = i + 1; j < a.size(); ++j) {
+            if (a[i] > a[j]) {
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+}
+
+int main() {
+    ds::array_list<int> a = {2, 3, 9, 8, 10, 98, 1, 2, -1, 3};
+
+    selectionSort(a);
+
+    for (auto &i : a) {
+        std::cout << i << " ";
+    }
 
     return 0;
 }
